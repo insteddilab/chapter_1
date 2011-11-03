6e4c173a542b7848ec6a607ff68067efecdf6bb5
Mann <chanmannlim@gmail.com>
2011-11-03 11:54:36 +0700

    stub test model

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe Customer do
+  let!(:movie) { Movie.new 'Inception', Movie::REGULAR }
+  let!(:rental) { Rental.new movie, 2 }
+
+  before(:each) do
+    @customer = Customer.new 'Kola'
+  end
+
+  it "should add rental" do
+    @customer.add_rental rental
+  end
+
+  describe "print statement" do
+
+  end
 end
