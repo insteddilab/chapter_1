8b6051cf7938d17eb797e5dde40efac0119769c4
Mann <chanmannlim@gmail.com>
2011-11-18 08:42:04 +0700

    add customer#statement tests

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-
 describe Customer do
   describe "print statement" do
     before(:each) do
@@ -41,6 +40,22 @@ describe Customer do
         [1, 3],
         [2, 6],
         [3, 9]
+      ].each do |day_rented, total_amount|
+        it "should total amount equal to #{total_amount} when rent for #{day_rented} (days)" do
+          expect_rental_amount movie, day_rented, total_amount
+        end
+      end
+    end
+
+    context "for children movie" do
+      let!(:movie) { Movie.new 'How to train your dragon', Movie::CHILDRENS }
+
+      [
+        [1, 1.5],
+        [2, 1.5],
+        [3, 1.5],
+        [4, 3],
+        [5, 4.5]
       ].each do |day_rented, total_amount|
         it "should total amount equal to #{total_amount} when rent for #{day_rented} (days)" do
           expect_rental_amount movie, day_rented, total_amount
