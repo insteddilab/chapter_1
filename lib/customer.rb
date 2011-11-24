c47093977109d813d69337c12cbfb13afe5e631d
Mann <chanmannlim@gmail.com>
2011-11-24 11:49:06 +0700

    refactor methods: get_charge, get_frequent_renters_point

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,24 +17,13 @@ class Customer
     frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
-      this_amount = 0;
-      case rental.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += rental.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      end
       # add frequent renter points
       frequent_renter_points += 1
       # add bonus for a two day new release rental
       frequent_renter_points += 1 if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
       # show figures for this rental
-      result << "\t#{rental.movie.title}\t#{this_amount}\n"
-      total_amount += this_amount
+      result << "\t#{rental.movie.title}\t#{rental.get_charge}\n"
+      total_amount += rental.get_charge
 
     end
     # add footer lines
--- lib/rental.rb
+++ lib/rental.rb
@@ -21,5 +21,18 @@ class Rental
     this_amount
 	end
 
-  
+  def get_charge
+    result = 0 
+    case self.movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (self.days_rented - 2) * 1.5 if self.days_rented > 2
+    when Movie::NEW_RELEASE
+      result += self.days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (self.days_rented - 3) * 1.5 if self.days_rented > 3
+    end
+   result
+  end
 end
